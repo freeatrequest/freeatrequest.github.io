2eb61a24b686baea0ad0dbb3c18d69efb95a0356
freeatrequest <free.atrequest@gmail.com>
2025-11-17 11:26:04 -0500

    Starting point and comments

---
+++ Assignment_4_part_2/script.js
@@ -0,0 +1,14 @@
+/*
+Name: Michael Mcdonald
+File: index.html
+Date: 18 November 2025
+INFT1206 – Web Development Fundamentals
+
+Image Gallery
+*/
+
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
