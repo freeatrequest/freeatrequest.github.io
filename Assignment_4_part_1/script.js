7a5c6c6c39274419206a6770cfe28e2f4204f74d
freeatrequest <free.atrequest@gmail.com>
2025-11-17 10:44:48 -0500

    Completed some steps
    Almost done assignment

--- Assignment_4_part_1/script.js
+++ Assignment_4_part_1/script.js
@@ -44,23 +44,28 @@ const randomEvent = randomValueFromArray(events)
  let storyText =  "It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomePlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day."
 
   return storyText;
-}
+} 
 
 // Event listener and partial generate function definition
 
 generateBtn.addEventListener("click", generateStory);
 
 function generateStory() {
+  let newStory = returnRandomStoryString()
+
   if (customName.value !== "") {
     const name = customName.value;
+    newStory = newStory.replace('Bob', name);
   }
 
   if (document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature = Math.round(94);
+    const weight = Math.round(300/14) + " stone";
+    const temperature = Math.round((94-32)*5/9) + " Celcius";
+    	newStory = newStory.replace("300 pounds", weight);
+	newStory = newStory.replace("94 Farenheit", temperature);	
   }
 
   // TODO: replace "" with the correct expression
-  story.textContent = "";
+  story.textContent = newStory;
   story.style.visibility = "visible";
 }
