227a957c2bbd4e1300036d59cd5546a62ed29caf
freeatrequest <free.atrequest@gmail.com>
2025-11-25 18:18:54 -0500

    Added pictures

--- Assignment_4_part_2/script.js
+++ Assignment_4_part_2/script.js
@@ -16,3 +16,11 @@ const overlay = document.querySelector(".overlay");
 const images = {
 
 }
+
+const images = [
+{filename: "pic1.jpg", alt: "Closeup of a human eye"},
+{filename: "pic2.jgp", alt: "Rock that looks like a wave"},
+{filename: "pic3.jpg", alt: "Purple and white pansies"},
+{filename: "pic4.jpg", alt: "Section of wall from a pharoh's tomb"},
+{filename: "pic5.jpg", alt: "large moth on a leaf"}
+];
