5e8b821b532d80cd959cb1477c1d661f2364a0d9
freeatrequest <free.atrequest@gmail.com>
2025-11-26 13:30:02 -0500

    Fixed some syntax

--- Assignment_4_part_2/script.js
+++ Assignment_4_part_2/script.js
@@ -16,23 +16,23 @@ const overlay = document.querySelector(".overlay");
 
 
 const images = [
-{filename: "pic1.jpg", alt: "Closeup of a human eye"},
-{filename: "pic2.jpg", alt: "Rock that looks like a wave"},
-{filename: "pic3.jpg", alt: "Purple and white pansies"},
-{filename: "pic4.jpg", alt: "Section of wall from a pharoh's tomb"},
-{filename: "pic5.jpg", alt: "large moth on a leaf"}
+	{filename: "pic1.jpg", alt: "Closeup of a human eye."},
+	{filename: "pic2.jpg", alt: "Rock that looks like a wave."},
+	{filename: "pic3.jpg", alt: "Purple and white pansies."},
+	{filename: "pic4.jpg", alt: "Section of wall from a pharoh's tomb."},
+	{filename: "pic5.jpg", alt: "large moth on a leaf."}
 ];
 
 const baseURL = 
 	"https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
 for (const image of images) {
 	const newimage = document.createElement("img");
-	newimage.src = `${baseURL}${image.filename}`;
-	newimage.alt = image.alt;
-	newimage.tabIndex = "0";
+	newImage.src = `${baseURL}${image.filename}`;
+	newImage.alt = image.alt;
+	newImage.tabIndex = "0";
 	thumbBar.appendChild(newimage);
-	newimage.addEventListener("click", updateDisplayedImage);
-	newimage.addEventListener("keydown", (e) => {
+	newImage.addEventListener("click", updateDisplayedImage);
+	newImage.addEventListener("keydown", (e) => {
 		if(e.code === "Enter") {
 			updateDisplayedImage(e);
 		}
@@ -45,10 +45,11 @@ function updateDisplayedImage(e) {
 }
 
 btn.addEventListener("click", () => {
-	if(btn.textContent === "Lighten";{
-	overlay.style.backgroundColor = "rgb(000/0.50";
+	if(btn.classList.contains("dark")){
+	btn.textContent === "Lighten";
+	overlay.style.backgroundColor = "rgb(0 0 0/ 0.50";
 } else {btn.textContent = "Darken";
-	overlay.style.backgroundColor = "rgb(000/0)";
+	overlay.style.backgroundColor = "rgb(0 0 0/ 0)";
 }
 btn.classList.toggle("dark");
 });
