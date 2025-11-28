42088d5c1c6ebc22d218718be6af5d15ad39f83d
freeatrequest <free.atrequest@gmail.com>
2025-11-27 19:29:01 -0500

    defining methods for evil circle

--- Assignment_4_part_4/main-finished.js
+++ Assignment_4_part_4/main-finished.js
@@ -27,16 +27,29 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Ball {
-  constructor(x, y, velX, velY, color, size) {
+class Shape {
+  constructor(x, y, velX, velY) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+	}
+}
+
+class Ball extends Shape {
+  constructor(x, y, velX, velY, color, size) {
+	super(x, y, velX, velY)  
+
     this.color = color;
     this.size = size;
+	this.exists = true;
   }
-
+class EvilCircle extends Shape {
+	constructor(x, y)
+	super(x, y, 20, 20);
+	this.color = white;
+	this.size = 10;
+}
   draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
@@ -66,14 +79,15 @@ class Ball {
   }
 
   collisionDetect() {
+  	  
     for (const ball of balls) {
-      if (!(this === ball)) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
 
         if (distance < this.size + ball.size) {
-          ball.color = this.color = randomRGB();
+          ball.color = this.color = randomRGB()
         }
       }
     }
@@ -81,6 +95,23 @@ class Ball {
 }
 
 const balls = [];
+
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "a":
+      this.x -= this.velX;
+      break;
+    case "d":
+      this.x += this.velX;
+      break;
+    case "w":
+      this.y -= this.velY;
+      break;
+    case "s":
+      this.y += this.velY;
+      break;
+  }
+});
 
 while (balls.length < 25) {
   const size = random(10, 20);
